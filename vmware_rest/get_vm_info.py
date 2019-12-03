51d6231842135110e39d2e5f7aac29f0f81f419e
Steven Carnes <scarnes@scarnes-MacBook-Pro.local>
2019-12-03 10:10:01 -0800

    moved the session management into a seperate script and import for use in other examples in the vmware_rest folder

--- vmware_rest/get_vm_info.py
+++ vmware_rest/get_vm_info.py
@@ -9,18 +9,23 @@
 import requests
 import json
 
+# import local library
+import manage_session
+
 # Here we are disabling the warning about Insecure Requests as we are using a self-signed certificate so we will be disabling the SSL Verification
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# The method we will use through most of the examples here is a GET, however we start with POST as explained later on below
-request_method = "POST"
+# Call get_session to get our session token 
+session_token = manage_session.get_session()
 
-# Lets define the URL we need to create our session
-url = "https://10.64.5.21/rest/com/vmware/cis/session"
+"""
 
-# for the session call we need to build out a new set of headers
-headers = {"Content-type": "application/json", "vmware-use-header-authn": None, "vmware-api-session-id": None}
+    Now, we can use the REST API's for vCenter to gather information on our environment
+
+"""
+
+# First, we need to establish the headers for our calls and the rest_method
 
 # Since we're probably using a self signed certificate I'm going to go ahead and disable SSL verification.
 # This will actually be used in our call below
@@ -32,33 +37,6 @@ password = "!Passw0rd"
 
 # The payload is empty in this case as the authentication used is all the session requires
 payload = {} 
-
-# Now we'll make the request and we'll store the results in a variable called 'request'
-# This is now an object that's going to have a lot of attributes associated with it
-response = requests.request(request_method,                     #
-                            url=url,
-                            headers=headers,
-                            verify=ssl_verify,
-                            data=json.dumps(payload),
-                            auth=(username, password))
-
-# retrieve the json response
-json_response = response.json()
-
-# then we retrieve the token from response
-session_token = json_response["value"]
-
-# As you can see here, we can print out our response and then store the session identifier for later use
-print("Here is our session token!")
-print(session_token)
-
-"""
-
-    Now, we can use the REST API's for vCenter to gather information on our environment
-
-"""
-
-# First, we need to establish the headers for our calls and the rest_method
 
 # we will be using GET for most of these calls because we are only retrieving data from our environment
 request_method = "GET"
@@ -102,21 +80,5 @@ print(json_pretty)
 
 """
 
-# We will use the DELETE verb to indicate our intentions here
-request_method = "DELETE"
-
-# The same url used to create our session
-url = "https://10.64.5.21/rest/com/vmware/cis/session"
-
-# for the session call we need to build out a new set of headers
-headers = {"Content-type": "application/json", "vmware-use-header-authn": None, "vmware-api-session-id": session_token}
-
-response = requests.request(request_method,                     #
-                            url=url,
-                            headers=headers,
-                            verify=ssl_verify,
-                            data=json.dumps(payload),
-                            auth=(username, password))
-
-if (response.status_code == 200):
-    print ('The session was terminated!')
+# delete our session token
+manage_session.delete_session(session_token)
--- vmware_rest/restart_vm.py
+++ vmware_rest/restart_vm.py
@@ -9,22 +9,21 @@
 import requests
 import json
 
+# import local library
+import manage_session
+
 # Here we are disabling the warning about Insecure Requests as we are using a self-signed certificate so we will be disabling the SSL Verification
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# The method we will use through most of the examples here is a GET, however we start with POST as explained later on below
-request_method = "POST"
+# Call get_session to get our session token 
+session_token = manage_session.get_session()
 
-# Lets define the URL we need to create our session
-url = "https://10.64.5.21/rest/com/vmware/cis/session"
-
-# for the session call we need to build out a new set of headers
-headers = {"Content-type": "application/json", "vmware-use-header-authn": None, "vmware-api-session-id": None}
 
 # Since we're probably using a self signed certificate I'm going to go ahead and disable SSL verification.
 # This will actually be used in our call below
 ssl_verify = False
+
 
 # The credentials to log into our device
 username = "administrator@vsphere.local"
@@ -32,25 +31,6 @@ password = "!Passw0rd"
 
 # The payload is empty in this case as the authentication used is all the session requires
 payload = {} 
-
-# Now we'll make the request and we'll store the results in a variable called 'request'
-# This is now an object that's going to have a lot of attributes associated with it
-response = requests.request(request_method,                     #
-                            url=url,
-                            headers=headers,
-                            verify=ssl_verify,
-                            data=json.dumps(payload),
-                            auth=(username, password))
-
-# retrieve the json response
-json_response = response.json()
-
-# then we retrieve the token from response
-session_token = json_response["value"]
-
-# As you can see here, we can print out our response and then store the session identifier for later use
-print("Here is our session token!")
-print(session_token)
 
 # we will be using GET for most of these calls because we are only retrieving data from our environment
 request_method = "GET"
@@ -90,21 +70,5 @@ if (response.status_code == 200):
 
 """
 
-# We will use the DELETE verb to indicate our intentions here
-request_method = "DELETE"
-
-# The same url used to create our session
-url = "https://10.64.5.21/rest/com/vmware/cis/session"
-
-# for the session call we need to build out a new set of headers
-headers = {"Content-type": "application/json", "vmware-use-header-authn": None, "vmware-api-session-id": session_token}
-
-response = requests.request(request_method,                     #
-                            url=url,
-                            headers=headers,
-                            verify=ssl_verify,
-                            data=json.dumps(payload),
-                            auth=(username, password))
-
-if (response.status_code == 200):
-    print ('The session was terminated!')
+# delete our session token
+manage_session.delete_session(session_token)
